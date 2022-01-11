513d27b2bfd31cd5797f892fdc3a4265789296f2
gleb_banniy <rusrandom@gmail.com>
2022-01-11 14:14:27 +0300

    implement controller and swagger ver3
    implement controller and swagger ver2
    
    implement controller and swagger

--- src/main/java/com/example/bidsender/BidSenderApplication.java
+++ src/main/java/com/example/bidsender/BidSenderApplication.java
@@ -1,13 +1,40 @@
 package com.example.bidsender;
 
+import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BidSenderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BidSenderApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any()).build().pathMapping("/")
+                .apiInfo(apiInfo()).useDefaultResponseMessages(false);
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("My Application API through Swagger UI")
+                .version("1.0")
+                .license("(C) Copyright Test")
+                .description("List of all the APIs of My Application App through Swagger UI")
+                .build();
+    }
 }
